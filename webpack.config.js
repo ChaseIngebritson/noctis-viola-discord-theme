6c742e814063933c824555754b307e328a589ef8
Chase Ingebritson <chase.ingebritson2014@gmail.com>
2021-02-17 00:11:59 -0600

    Updated dependencies, changed deployment structure

--- webpack.config.js
+++ webpack.config.js
@@ -5,36 +5,50 @@ const StylelintPlugin = require('stylelint-webpack-plugin');
 
 const package = require("./package.json");
 
+const plugins = [
+  new FixStyleOnlyEntriesPlugin(),
+  new MiniCssExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // all options are optional
+    filename: '[name].css',
+    chunkFilename: '[id].css',
+    ignoreOrder: false, // Enable to remove warnings about conflicting order
+  }),
+  new webpack.BannerPlugin({
+    banner: `/**
+  * @name        ${package.name}
+  * @description ${package.description}
+  * @author      ${package.author}
+  * @authorId    ${package.authorId}
+  * @version     ${package.version}
+  * @website     ${package.repository.url}
+  * @source      ${package.homepage}/noctis-viola.css
+  */\n`,
+    raw: true,
+    include: 'noctis-viola.theme.css'
+  }),
+  new StylelintPlugin({
+    context: 'src/styles'
+  })
+]
+
+if (process.env.NODE_ENV === 'development') {
+  plugins.push(new webpack.HotModuleReplacementPlugin())
+}
+
 module.exports = {
-  entry : "./src/styles/noctis-viola.theme.scss",
-  plugins: [
-    new FixStyleOnlyEntriesPlugin(),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // all options are optional
-      filename: 'noctis-viola.theme.css',
-      chunkFilename: '[id].css',
-      ignoreOrder: false, // Enable to remove warnings about conflicting order
-    }),
-    new webpack.BannerPlugin({
-      banner: `//META{"name":"${package.name}","description":"${package.description}","author":"${package.author}","authorId":"${package.authorId}","version":"${package.version}","website":"${package.homepage}"}*//\n`,
-      raw: true,
-      include: 'noctis-viola.theme.css'
-    }),
-    new StylelintPlugin({
-      context: 'src/styles'
-    })
-  ],
+  entry: {
+    "noctis-viola": "./src/styles/noctis-viola.theme.scss",
+    "noctis-viola.theme": "./src/import.scss"
+  },
+  plugins,
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: process.env.NODE_ENV === 'development',
-            },
+            loader: MiniCssExtractPlugin.loader
           },
           // Translates CSS into CommonJS
           'css-loader',
